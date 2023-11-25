c2c9f65e428d59f7ab2bd76968cbd47742e56cdb
JustWhit3 <biancogianluca9@gmail.com>
2023-11-26 00:35:06 +0100

    Updates in the data processing of simulated data

--- data/processing.py
+++ data/processing.py
@@ -7,129 +7,160 @@
 
 # Simulated process: gg -> tt~ -> l l~ nu nu~ b b~
 
-# TODO: parallelizza
-# TODO: unità di misura
-# TODO: perchè alcuni eventi hanno solo un e o solo un mu?
+# TODO: introdurre bias?
+# TODO: barre di errore
 
 # STD libraries
 import argparse as ap
 from tqdm import tqdm
-import sys
+from array import array
 
 # Data science modules
 import uproot
-import numpy as np
 import awkward as ak
+import ROOT
 
 
-def make_particle_tree():
-    """
-    Create the particle-level tree to be added in the output root file.
+def get_lep_pT_at_particle_level(event):
+    lepton_mask = (abs(event["Particle.PID"]) == 11) | (abs(event["Particle.PID"]) == 13)
+    sorted_leptons_pT = ak.argsort(event["Particle.PT"][lepton_mask], axis=-1, ascending=False)
+    leading_lepton_pT = event["Particle.PT"][lepton_mask][sorted_leptons_pT[0]]
+    subleading_lepton_pT = event["Particle.PT"][lepton_mask][sorted_leptons_pT[1]]
 
-    Returns:
-        dict: list of branches of the new particle-level tree.
-    """
+    return leading_lepton_pT, subleading_lepton_pT
 
+
+def get_trees_info(reco_file, particle_file):
     # Read ROOT file and trees
-    print("\nProcessing {} file to make particle branch...".format(args.particle))
-    file = uproot.open(args.particle)
-    tree = file["LHEF"]
+    file_reco = uproot.open(reco_file)
+    tree_reco = file_reco["Delphes"]
+    reco_info = tree_reco.arrays(["Jet.Flavor", "Jet.PT", "Electron.PT", "Muon.PT"])
 
-    # Filter particle level and get useful information
-    particle_level = tree["Particle"]
+    reco_pT_lep1_list = []
+    reco_pT_lep2_list = []
+
+    file_particle = uproot.open(particle_file)
+    tree_particle = file_particle["LHEF"]
+    particle_level = tree_particle["Particle"]
     particle_info = particle_level.arrays(["Particle.PT", "Particle.PID"])
 
-    # Variables
-    leading_leptons_pt_list = []
-    subleading_leptons_pt_list = []
+    particle_pT_lep1_list = []
+    particle_pT_lep2_list = []
 
     # Iterate over events
-    for event in tqdm(particle_info, ncols=100):
-        lepton_mask = (abs(event["Particle.PID"]) == 11) | (
-            abs(event["Particle.PID"]) == 13
-        )
-
-        # Get leading and subleading lepton pT
-        sorted_leptons_pT = ak.argsort(
-            event["Particle.PT"][lepton_mask], axis=-1, ascending=False
-        )
-        leading_lepton_pT = event["Particle.PT"][lepton_mask][sorted_leptons_pT[0]]
-        leading_leptons_pt_list.append(leading_lepton_pT)
-        subleading_lepton_pT = event["Particle.PT"][lepton_mask][sorted_leptons_pT[1]]
-        subleading_leptons_pt_list.append(subleading_lepton_pT)
-
-    # Save information into particle tree
-    particle_tree = {
-        "pT_lep1": ak.from_iter(leading_leptons_pt_list),
-        "pT_lep2": ak.from_iter(subleading_leptons_pt_list),
-    }
-
-    return particle_tree
-
-
-def make_reco_tree():
-    """
-    Create the reco-level tree to be added in the output root file.
-
-    Returns:
-        dict: list of branches of the new reco-level tree.
-    """
-
-    # Read ROOT file and trees
-    print("\nProcessing {} file to make reco branch...".format(args.reco))
-    file = uproot.open(args.reco)
-    tree = file["Delphes"]
-    reco_info = tree.arrays(["Jet.Flavor", "Jet.PT", "Electron.PT", "Muon.PT"])
-
-    # Variables
-    leading_leptons_pt_list = []
-    subleading_leptons_pt_list = []
-
-    # Iterate over events
-    for event in tqdm(reco_info, ncols=100):
+    for reco_event, particle_event in tqdm(
+        zip(reco_info, particle_info), total=len(reco_info), ncols=100
+    ):
         # Get leading and subleading leptons pT
-        if len(event["Electron.PT"]) == 2 and len(event["Muon.PT"]) == 0:
-            sorted_leptons_pT = ak.argsort(
-                event["Electron.PT"], axis=-1, ascending=False
-            )
-            leading_lepton_pT = event["Electron.PT"][sorted_leptons_pT[0]]
-            leading_leptons_pt_list.append(leading_lepton_pT)
-            subleading_lepton_pT = event["Electron.PT"][sorted_leptons_pT[1]]
-            subleading_leptons_pt_list.append(subleading_lepton_pT)
-        elif len(event["Muon.PT"]) == 2 and len(event["Electron.PT"]) == 0:
-            sorted_leptons_pT = ak.argsort(event["Muon.PT"], axis=-1, ascending=False)
-            leading_lepton_pT = event["Muon.PT"][sorted_leptons_pT[0]]
-            leading_leptons_pt_list.append(leading_lepton_pT)
-            subleading_lepton_pT = event["Muon.PT"][sorted_leptons_pT[1]]
-            subleading_leptons_pt_list.append(subleading_lepton_pT)
-        elif len(event["Electron.PT"]) == 1 and len(event["Muon.PT"]) == 1:
-            if event["Electron.PT"][0] > event["Muon.PT"][0]:
-                leading_leptons_pt_list.append(event["Electron.PT"][0])
-                subleading_leptons_pt_list.append(event["Muon.PT"][0])
-            elif event["Electron.PT"][0] < event["Muon.PT"][0]:
-                leading_leptons_pt_list.append(event["Muon.PT"][0])
-                subleading_leptons_pt_list.append(event["Electron.PT"][0])
-        else:
-            print("A")
+        if len(reco_event["Electron.PT"]) == 2 and len(reco_event["Muon.PT"]) == 0:
+            # Reco
+            sorted_leptons_pT = ak.argsort(reco_event["Electron.PT"], axis=-1, ascending=False)
+            reco_pT_lep1_list.append(reco_event["Electron.PT"][sorted_leptons_pT[0]])
+            reco_pT_lep2_list.append(reco_event["Electron.PT"][sorted_leptons_pT[1]])
+
+            # Particle
+            particle_pT_lep1, particle_pT_lep2 = get_lep_pT_at_particle_level(particle_event)
+            particle_pT_lep1_list.append(particle_pT_lep1)
+            particle_pT_lep2_list.append(particle_pT_lep2)
+
+        elif len(reco_event["Muon.PT"]) == 2 and len(reco_event["Electron.PT"]) == 0:
+            # Reco
+            sorted_leptons_pT = ak.argsort(reco_event["Muon.PT"], axis=-1, ascending=False)
+            reco_pT_lep1_list.append(reco_event["Muon.PT"][sorted_leptons_pT[0]])
+            reco_pT_lep2_list.append(reco_event["Muon.PT"][sorted_leptons_pT[1]])
+
+            # Particle
+            particle_pT_lep1, particle_pT_lep2 = get_lep_pT_at_particle_level(particle_event)
+            particle_pT_lep1_list.append(particle_pT_lep1)
+            particle_pT_lep2_list.append(particle_pT_lep2)
+        elif len(reco_event["Electron.PT"]) == 1 and len(reco_event["Muon.PT"]) == 1:
+            if reco_event["Electron.PT"][0] > reco_event["Muon.PT"][0]:
+                # Reco
+                reco_pT_lep1_list.append(reco_event["Electron.PT"][0])
+                reco_pT_lep2_list.append(reco_event["Muon.PT"][0])
+
+                # Particle
+                particle_pT_lep1, particle_pT_lep2 = get_lep_pT_at_particle_level(particle_event)
+                particle_pT_lep1_list.append(particle_pT_lep1)
+                particle_pT_lep2_list.append(particle_pT_lep2)
+            elif reco_event["Electron.PT"][0] < reco_event["Muon.PT"][0]:
+                # Reco
+                reco_pT_lep1_list.append(reco_event["Muon.PT"][0])
+                reco_pT_lep2_list.append(reco_event["Electron.PT"][0])
+
+                # Particle
+                particle_pT_lep1, particle_pT_lep2 = get_lep_pT_at_particle_level(particle_event)
+                particle_pT_lep1_list.append(particle_pT_lep1)
+                particle_pT_lep2_list.append(particle_pT_lep2)
+
+    # Binning
+    # fmt: off
+    pT_lep1_binning = [28.00, 34.00, 39.00, 45.00, 52.00, 61.00, 71.00, 83.00, 97.00, 115.00, 134.00, 158.00, 188.00, 223.00, 268.00, 338.00, 400.00]
+    pT_lep2_binning = [28.00, 32.00, 37.00, 44.00, 51.00, 61.00, 73.00, 88.00, 105.00, 123.00, 150.00, 182.00, 223.00, 268.00, 338.00, 400.00]
 
     # Save information into particle tree
     reco_tree = {
-        "pT_lep1": ak.from_iter(leading_leptons_pt_list),
-        "pT_lep2": ak.from_iter(subleading_leptons_pt_list),
+        "pT_lep1": [ak.from_iter(reco_pT_lep1_list), pT_lep1_binning],
+        "pT_lep2": [ak.from_iter(reco_pT_lep2_list), pT_lep2_binning],
     }
 
-    return reco_tree
+    particle_tree = {
+        "pT_lep1": [ak.from_iter(particle_pT_lep1_list), pT_lep1_binning],
+        "pT_lep2": [ak.from_iter(particle_pT_lep2_list), pT_lep2_binning],
+    }
+
+    return reco_tree, particle_tree
 
 
 def main():
     # Create trees
-    reco_level = make_reco_tree()
-    particle_level = make_particle_tree()
+    print("\nCreating particle- and reco-level trees:")
+    print("- Reco file: {}".format(args.reco))
+    print("- Particle file: {}".format(args.particle))
+    reco_level, particle_level = get_trees_info(args.reco, args.particle)
 
-    # Save output
-    output = uproot.recreate(args.output)
-    output["reco"] = reco_level
-    output["particle"] = particle_level
+    # Save output trees
+    output = ROOT.TFile(args.output, "RECREATE")
+    reco_dir = output.mkdir("reco")
+    particle_dir = output.mkdir("particle")
+    for h_reco, h_particle in zip(reco_level.keys(), particle_level.keys()):
+        binning = reco_level[h_reco][1]
+        bins = len(binning) - 1
+        reco_histo = ROOT.TH1D(h_reco, h_reco, bins, array("d", binning))
+        particle_name = "particle_{}".format(h_particle)
+        particle_histo = ROOT.TH1D(particle_name, particle_name, bins, array("d", binning))
+        x = ak.to_numpy(reco_level[h_reco][0])
+        y = ak.to_numpy(particle_level[h_particle][0])
+        for i in range(len(x)):
+            reco_histo.Fill(x[i])
+            particle_histo.Fill(y[i])
+        reco_dir.cd()
+        reco_histo.Write()
+        particle_dir.cd()
+        particle_histo.Write()
+
+    # Create response matrices data
+    print("\nCreating response matrices:")
+    print("- Reco file: {}".format(args.reco_response))
+    print("- Particle file: {}".format(args.particle_response))
+    res_reco_level, res_particle_level = get_trees_info(args.reco_response, args.particle_response)
+
+    # Create and save response matrices
+    for h_reco, h_particle in zip(res_reco_level.keys(), res_particle_level.keys()):
+        response_name = "particle_{0}_vs_{0}".format(h_reco)
+        binning = reco_level[h_reco][1]
+        bins = len(binning) - 1
+        response = ROOT.TH2D(
+            response_name, response_name, bins, array("d", binning), bins, array("d", binning)
+        )
+        x = ak.to_numpy(res_reco_level[h_reco][0])
+        y = ak.to_numpy(res_particle_level[h_particle][0])
+        for i in range(len(x)):
+            response.Fill(x[i], y[i])
+        response.Draw("colz")
+        reco_dir.cd()
+        response.Write()
+    output.Close()
 
 
 if __name__ == "__main__":
